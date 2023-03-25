f17264dfd859a442651386e911feca645c7b69c9
EpicDima <>
2023-03-26 00:55:48 +0300

    Solution #2

--- buildSrc/src/main/java/Modules.kt
+++ buildSrc/src/main/java/Modules.kt
@@ -1,8 +1,10 @@
-package com.epicdima
-
 object Modules {
     const val lib = ":library"
     const val libkts = ":librarykts"
+
+    // the field name must be equal to the object name. it doesn't matter private or public.
+    // if public, you can use the field instead of referring to object
+    val dependencies = Dependencies
 
     object Dependencies {
         const val dep = ":dependency"
--- librarykts/build.gradle.kts
+++ librarykts/build.gradle.kts
@@ -1,5 +1,3 @@
-import com.epicdima.Modules
-
 plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
