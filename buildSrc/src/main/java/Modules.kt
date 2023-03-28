16cd8ec119c619b81163e056edcdc35ea0bae313
EpicDima <>
2023-03-28 11:31:24 +0300

    Solution #4

--- buildSrc/src/main/java/Modules.kt
+++ buildSrc/src/main/java/Modules.kt
@@ -2,9 +2,11 @@ object Modules {
     const val lib = ":library"
     const val libkts = ":librarykts"
 
-    // works only for build.gradle (does not work with buidl.gradle.kts)
-    val Dependencies = object {
-        val dep = ":dependency"
-        val depkts = ":dependencykts"
+    @JvmField
+    val Dependencies = _Dependencies
+
+    object _Dependencies {
+        const val dep = ":dependency"
+        const val depkts = ":dependencykts"
     }
 }
